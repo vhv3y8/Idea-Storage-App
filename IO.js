e1bede7f3378ea1887a975f97c0a54d4dc540b6c
ks2019875067 <48702265+ks2019875067@users.noreply.github.com>
2020-07-02 11:22:32 +0900

    Update IO.js

--- IO.js
+++ IO.js
@@ -4,7 +4,21 @@ import { View, Text } from "react-native";
 export default class IO extends React.Component {
 
     static input = (JSONdata) => {
-        console.log(JSONdata);
+        key.DB = JSONdata; //input this JSON
+        console.log(complete input); //return complete-signal
+    };
+    static delete = (key) => {
+        key.DB = null; // delete file
+        console.log(complete delete); //return complete-signal
+    };
+    static tag = (tagJSON) => {
+        console.log(JSONdatas); // return JSON in tagging datas 
+    };
+    static edit = (key) => {
+        console.log(keyJSON); // return this JSON
+    };
+    static All = () => {
+        consol.log(DBJSON); //return JSON in All datas
     };
 
     render() {
